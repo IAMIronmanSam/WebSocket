b89ed748e70f5f0e33dc6ea022520265b39131a2
Arivazhagan <arivu86@gmail.com>
2012-12-30 13:47:43 +0530

    HouseKeeping

--- src/main/java/com/test/websocket/HelloWebSocket.java
+++ src/main/java/com/test/websocket/HelloWebSocket.java
@@ -4,9 +4,15 @@
  */
 package com.test.websocket;
 
+import javax.websocket.DecodeException;
+import javax.websocket.Decoder;
+import javax.websocket.EncodeException;
+import javax.websocket.Encoder;
 import javax.websocket.EndpointFactory;
 import javax.websocket.WebSocketEndpoint;
 import javax.websocket.WebSocketMessage;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
 /**
  *
@@ -33,3 +39,35 @@ public class HelloWebSocket {
             return null;
         }
     }
+
+    class FigureEncoder implements Encoder.Text<Figure> {
+    @Override
+    public String encode(Figure figure) throws EncodeException {
+        return figure.getJson().toString();
+    }
+}
+
+    class FigureDecoder implements Decoder.Text<Figure> {
+    @Override
+    public Figure decode(String string) throws DecodeException {
+        try {
+            System.out.println("decoding: " + string);
+            JSONObject jsonObject = new JSONObject(string);
+            return new Figure(jsonObject);
+        } catch (JSONException ex) {
+            throw new DecodeException("Error parsing JSON", ex.getMessage(), ex.fillInStackTrace());
+        }
+    }
+
+    @Override
+    public boolean willDecode(String string) {
+        try {
+            new JSONObject(string);
+            return true;
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+    
+}
